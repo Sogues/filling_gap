6cd289c1d9f0da69d9579776fdeb24ce831b4f08
Sogues <mail.caoke@icloud.com>
2019-08-16 00:49:56 +0800

    容器-红黑树-删除

--- gap_container/rb_tree_ctype.c
+++ gap_container/rb_tree_ctype.c
@@ -68,6 +68,176 @@ void rb_tree_insert(rb_tree_t* tree, rb_tree_node_t* node)
     SET_RB_TREE_NODE_BLACK(*root);
 }
 
+void rb_tree_delete(rb_tree_t* tree, rb_tree_node_t* node)
+{
+    rb_tree_node_t **root, *sentinel, *del, *replace, *tmp;
+    int red;
+    root = &tree->root;
+    sentinel = tree->sentinel;
+
+    if (sentinel == node->left)
+    {
+        del = node;
+        replace = node->right;
+    }
+    else if (sentinel == node->right)
+    {
+        del = node;
+        replace = node->left;
+    }
+    else
+    {
+        del = rb_tree_min_right(node->right, sentinel);
+        replace = del->right;
+    }
+
+    if (*root == del)
+    {
+        *root = replace;
+        SET_RB_TREE_NODE_BLACK(replace);
+        replace->parent = NULL;
+        node->parent = NULL;
+        node->left = NULL;
+        node->right = NULL;
+        node->key = NULL;
+        return;
+    }
+
+    red = IS_RB_TREE_NODE_RED(del);
+
+    if (del == del->parent->left)
+    {
+        del->parent->left = replace;
+    }
+    else
+    {
+        del->parent->right = replace;
+    }
+
+    if (del == node)
+    {
+        replace->parent = del->parent;
+    }
+    else
+    {
+        /// 避免父节点是要删除的节点
+        if (node == del->parent)
+        {
+            replace->parent = del;
+        }
+        else
+        {
+            replace->parent = del->parent;
+        }
+
+        del->parent = node->parent;
+        del->left = node->left;
+        del->right = node->right;
+        RB_TREE_COPY_COLOR(del, node);
+
+        if (node == *root)
+        {
+            *root = replace;
+        }
+        else
+        {
+            if (node == node->parent->left)
+            {
+                node->parent->left = del;
+            }
+            else
+            {
+                node->parent->right - del;
+            }
+        }
+
+        if (sentinel != del->left)
+        {
+            del->left->parent = del;
+        }
+        if (sentinel != del->right)
+        {
+            del->right->parent = del;
+        }
+    }
+
+    node->parent = NULL;
+    node->left = NULL;
+    node->right = NULL;
+    node->key = NULL;
+    if (red)
+    {
+        return;
+    }
+
+    while (replace != root && !IS_RB_TREE_NODE_RED(replace))
+    {
+        if (replace == replace->parent->left)
+        {
+            tmp = replace->parent->right;
+            if (IS_RB_TREE_NODE_RED(tmp))
+            {
+                SET_RB_TREE_NODE_BLACK(tmp);
+                SET_RB_TREE_NODE_RED(replace->parent);
+                rb_tree_left_rotate(root, replace->parent, sentinel);
+                tmp = replace->parent->right;
+            }
+            if (!IS_RB_TREE_NODE_RED(tmp->left) && !IS_RB_TREE_NODE_RED(tmp->right))
+            {
+                SET_RB_TREE_NODE_RED(tmp);
+                replace = replace->parent;
+            }
+            else
+            {
+                if (IS_RB_TREE_NODE_RED(tmp->left))
+                {
+                    SET_RB_TREE_NODE_BLACK(tmp->left);
+                    SET_RB_TREE_NODE_RED(tmp);
+                    rb_tree_right_rotate(root, tmp, sentinel);
+                    tmp = replace->parent->right;
+                }
+                RB_TREE_COPY_COLOR(tmp, replace->parent);
+                SET_RB_TREE_NODE_BLACK(replace->parent);
+                SET_RB_TREE_NODE_BLACK(tmp->right);
+                rb_tree_left_rotate(root, replace->parent, sentinel);
+                replace = *root;
+            }
+        }
+        else
+        {
+            tmp = replace->parent->left;
+            if (IS_RB_TREE_NODE_RED(tmp))
+            {
+                SET_RB_TREE_NODE_BLACK(tmp);
+                SET_RB_TREE_NODE_RED(replace->parent);
+                rb_tree_right_rotate(root, replace->parent, sentinel);
+                tmp = replace->parent->left;
+            }
+            if (!IS_RB_TREE_NODE_RED(tmp->left) && !IS_RB_TREE_NODE_RED(tmp->right))
+            {
+                SET_RB_TREE_NODE_RED(tmp);
+                replace = replace->parent;
+            }
+            else
+            {
+                if (IS_RB_TREE_NODE_RED(tmp->right))
+                {
+                    SET_RB_TREE_NODE_BLACK(tmp->right);
+                    SET_RB_TREE_NODE_RED(tmp);
+                    rb_tree_left_rotate(root, replace->parent, sentinel);
+                    tmp = replace->parent->left;
+                }
+                RB_TREE_COPY_COLOR(tmp, replace->parent);
+                SET_RB_TREE_NODE_BLACK(replace->parent);
+                SET_RB_TREE_NODE_BLACK(tmp->left);
+                rb_tree_right_rotate(root, tmp->parent, sentinel);
+                replace = *root;
+            }
+        }
+    }
+    SET_RB_TREE_NODE_BLACK(replace);
+}
+
 void rb_tree_left_rotate(rb_tree_node_t** root, rb_tree_node_t* node, rb_tree_node_t* sentinel)
 {
     rb_tree_node_t* right = node->right;
@@ -116,4 +286,13 @@ void rb_tree_right_rotate(rb_tree_node_t** root, rb_tree_node_t* node, rb_tree_n
         node->parent->right = left;
     }
     node->parent = left;
+}
+
+rb_tree_node_t* rb_tree_min_right(rb_tree_node_t* node, rb_tree_node_t* sentinel)
+{
+    while (sentinel != node->left)
+    {
+        node = node->left;
+    }
+    return node;
 }
--- gap_container/rb_tree_ctype.h
+++ gap_container/rb_tree_ctype.h
@@ -9,7 +9,7 @@ typedef int rb_node_color_t;
 #define rb_node_color_Black 0
 #define rb_node_color_red 1
 
-void (*rb_tree_insert_pt)(rb_tree_node_t* root, rb_tree_node_t* node, rb_tree_node_t* sentinel);
+typedef void (*rb_tree_insert_pt)(rb_tree_node_t* root, rb_tree_node_t* node, rb_tree_node_t* sentinel);
 
 typedef struct
 {
@@ -38,6 +38,8 @@ typedef struct
 #define IS_RB_TREE_NODE_RED(node) ((node)->color == rb_node_color_red)
 #define SET_RB_TREE_NODE_RED(node) (node)->color = rb_node_color_red
 #define SET_RB_TREE_NODE_BLACK(node) (node)->color = rb_node_color_Black
+#define RB_TREE_COPY_COLOR(from, to) (to)->color = (from)->color
 
 void rb_tree_insert(rb_tree_t* tree, rb_tree_node_t* node);
+void rb_tree_delete(rb_tree_t* tree, rb_tree_node_t* node);
 #endif
